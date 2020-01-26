0911d3c7a5923037df56ca9ac80f634b1a0c8950
Ricardo Falcão <ricardomanuelfalcao20@gmail.com>
2020-01-26 15:39:28 +0000

    dev

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   content: ["./_site/**/*.html"],
   css: ["./_site/assets/css/site.css"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  // Tailwind CSS config
+  extractors: [{
+    extractor: class {
+      static extract(content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+      }
+    },
+    extensions: ["html"]
+  }]
 };
