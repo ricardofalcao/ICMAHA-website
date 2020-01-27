97b643fae6ecc96dfe423cf4c3975de4622ee144
Ricardo Falcão <ricardomanuelfalcao20@gmail.com>
2020-01-27 14:01:52 +0000

    dev

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,8 +3,17 @@ function toggleNavbar(id) {
     x.classList.toggle("hidden");
 }
 
-var myNav = document.getElementById('example-navbar');
-window.onscroll = function () { 
+function showDropdown(id) {
+    var x = document.getElementById(id);
+    x.classList.remove("hidden");
+}
+
+function hideDropdown(id) {
+    var x = document.getElementById(id);
+    x.classList.add("hidden");
+}
+
+function fadeNavbar() { 
     "use strict";
     if (document.documentElement.scrollTop >= 200 ) {
         myNav.classList.add("lg:bg-gray-800");
@@ -15,3 +24,9 @@ window.onscroll = function () {
         myNav.classList.remove("lg:bg-gray-800");
     }
 };
+
+var myNav = document.getElementById('example-navbar');
+window.onscroll = fadeNavbar;
+window.onhashchange = fadeNavbar;
+
+fadeNavbar();
